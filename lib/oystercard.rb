99191781b0bab522c4950abc5ba8d9f8d0a8b714
Alessa <alessa_hardwick@live.co.uk>
2020-06-16 14:25:08 +0000

    added maximum balance constant

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
     attr_reader :balance
+    MAX_BALANCE = 90
     
     def initialize
         @balance = 0
     end
 
     def top_up(amount)
-        @balance = @balance + amount
+      fail 'exceeded balance' if (@balance + amount) > MAX_BALANCE
+      @balance += amount
     end
     
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,9 +17,20 @@ describe Oystercard do
     end
     
      describe '#top_up' do
-      it { is_expected.to respond_to(:top_up).with(1).argument }
-      it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+     # it { is_expected.to respond_to(:top_up).with(1).argument }
+      #it 'can top up the balance' do
+      #expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      #end
+      it 'adds money to the balance' do
+        oystercard = Oystercard.new
+        oystercard.top_up(3)
+        expect { oystercard.top_up(3) }.to change { oystercard.balance }.by(3)
+      end
+      it 'raises an error if the balance exceeds 90' do
+        oystercard = Oystercard.new
+        max_balance = Oystercard::MAX_BALANCE
+        oystercard.top_up(max_balance)
+        expect { oystercard.top_up(1) }.to raise_error 'exceeded balance'
       end
     end
 end
